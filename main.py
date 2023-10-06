352f2ba70b6f228ac6c233c6efd0563ab56eebe8
dixrow <castromarcos1999@gmail.com>
2023-10-06 20:52:11 +0200

    Added interval

--- main.py
+++ main.py
@@ -56,22 +56,28 @@ c1 = st.selectbox(
    placeholder="Select currency pair...",
 )
 
+#dropdown to select interval
+c2 = st.selectbox(
+    "Please select interval",
+    ("1", "5", "15", "30", "60", "240", "1440", "10080", "21600"),
+    index=None,
+    placeholder="Select interval...",
+)
+
 #button to generate graph
 if st.button('Plot it!'):
-    fig = generate_graph(pair=c1, interval=1440)
+    fig = generate_graph(pair=c1, interval=c2)
     st.pyplot(fig)
 
 
-"""
-Las variables son:
-    timestamp = data[0]
-    date_time = datetime.utcfromtimestamp(timestamp)
-    date = date_time.strftime('%Y-%m-%d')
-    time = date_time.strftime('%H:%M:%S')
-    open_price = data[1]
-    high = data[2]
-    low = data[3]
-    close = data[4]
-    volume = data[6]
-"""
+# Las variables son:
+#     timestamp = data[0]
+#     date_time = datetime.utcfromtimestamp(timestamp)
+#     date = date_time.strftime('%Y-%m-%d')
+#     time = date_time.strftime('%H:%M:%S')
+#     open_price = data[1]
+#     high = data[2]
+#     low = data[3]
+#     close = data[4]
+#     volume = data[6]
 # Fórmula del oscilador estocástico, %K = (U - Mi) / (Max - Mi) x 100  (U - cierre, Mi - mínimo, Max - máximo)
