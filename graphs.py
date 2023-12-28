9697b4e2e831d355d38c61376cdca468b294215e
Rodrigo de la Nuez Moraleda <rdelanuezmo@alumni.unav.es>
2023-12-28 22:42:16 +0100

    leyenda + profit

--- graphs.py
+++ graphs.py
@@ -94,16 +94,16 @@ class Graph:
             fig = make_subplots(specs=[[{"secondary_y": True}]])
 
             # Include candlestick with range selector
-            fig.add_trace(go.Candlestick(x=df.index, open=df['Open'], high=df['High'], low=df['Low'], close=df['Close'], name=''), secondary_y=True)
+            fig.add_trace(go.Candlestick(x=df.index, open=df['Open'], high=df['High'], low=df['Low'], close=df['Close'], name='', legendgroup='group', legendrank=1), secondary_y=True)
             
             # Bar diagram displaying the Volume data
             fig.add_trace(go.Bar(x=df.index, y=df['Volume'], marker_color=colors, opacity=0.25, showlegend=False), secondary_y=False)
             
             # Line chart displaying the computed SMA values
-            fig.add_trace(go.Scatter(x=df.index, y=df['SMA'], marker=dict(color='#0000FF'), opacity=0.35, name='SMA'), secondary_y=True)
+            fig.add_trace(go.Scatter(x=df.index, y=df['SMA'], marker=dict(color='#0000FF'), opacity=0.35, name='SMA', legendgroup='group', legendrank=2), secondary_y=True)
             
             # Line chart displaying the computed EMA values
-            fig.add_trace(go.Scatter(x=df.index, y=df['EMA'], marker=dict(color='#FF0000'), opacity=0.35, name='EMA'), secondary_y=True)
+            fig.add_trace(go.Scatter(x=df.index, y=df['EMA'], marker=dict(color='#FF0000'), opacity=0.35, name='EMA', legendgroup='group', legendrank=3), secondary_y=True)
             
             fig.layout.yaxis2.showgrid = False
             fig.layout.title = 'Candlestick Graph with Volume and Moving Averages'
@@ -130,11 +130,11 @@ class Graph:
             df['Sell_Signal'] = ((df['%K'] < df['%D']) & (df['%K'].shift(1) > df['%D'].shift(1))) & (df['%D'] > 80)
             df = df[14:]
 
-            data = [# The first plot is a line chart for the '%K' line of the stochastic oscillator
-                    go.Scatter(x=df.index, y=df['%K'], name='Stochastic Oscillator', marker=dict(color='#1E90FF')),
-
-                    # The second plot is a line chart for the '%D' line of the stochastic oscillator
-                    go.Scatter(x=df.index, y=df['%D'], name='Smoothed Stochastic', marker=dict(color='#87CEEB'))]
+            data = [# The first plot is a line chart for the '%D' line of the stochastic oscillator
+                    go.Scatter(x=df.index, y=df['%D'], name='Smoothed Stochastic', marker=dict(color='#b2b2b2'), legendgroup='group', legendrank=5),
+                    
+                    # The second plot is a line chart for the '%K' line of the stochastic oscillator
+                    go.Scatter(x=df.index, y=df['%K'], name='Stochastic Oscillator', marker=dict(color='#4c4c4c'), legendgroup='group', legendrank=4)]
 
             # Define the layout for the plotly figure, setting titles and axis labels.
             layout = go.Layout(title='Stochastic Oscillator with its Smoothed Version',
